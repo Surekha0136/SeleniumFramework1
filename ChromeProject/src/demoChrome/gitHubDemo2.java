018f89db3b8b69973f72f000da574ab07981734f
Shankar_Kakde <Shankar_Kakde@W102023FH2.blr.amer.dell.com>
2021-04-20 08:50:38 +0530

    GitHub Demo2 is added now

---
+++ ChromeProject/src/demoChrome/gitHubDemo2.java
@@ -0,0 +1,10 @@
+package demoChrome;
+
+public class gitHubDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Bangalore is hot in summer");
+	}
+
+}
