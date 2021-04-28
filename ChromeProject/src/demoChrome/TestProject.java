4f06d98a0e719822a4ccda98f81ca8f4992d03e5
Shankar_Kakde <Shankar_Kakde@W102023FH2.blr.amer.dell.com>
2021-04-28 18:17:18 +0530

    new commit on 28th April

---
+++ ChromeProject/src/demoChrome/TestProject.java
@@ -0,0 +1,10 @@
+package demoChrome;
+
+public class TestProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("covid -19 vaccine for 18+");
+	}
+
+}
